82627030290256d596673d6f091c79403c025c98
yourlastbreathe <djentelman92@mail.ru>
2022-10-22 15:59:01 +0300

    length

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+//функция, возвращающая случайное целое число из переданного диапазона включительно взята из developer.mozilla.org
+
+function checkStringLength(string, minLength, maxLength) {
+  return string.length <= maxLength && string.length >= minLength;
+}
