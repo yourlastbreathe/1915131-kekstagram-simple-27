0d13308171c833faf02fd972e88de0b5a2a389af
yourlastbreathe <djentelman92@mail.ru>
2022-11-05 11:58:04 +0300

    module5-task1

--- js/data.js
+++ js/data.js
@@ -1,9 +1,4 @@
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-getRandomIntInclusive(1, 9); //функция, возвращающая случайное целое число из переданного диапазона включительно взята из developer.mozilla.org
+import {getRandomIntInclusive} from './util.js';
 
 const photos = [];
 const createPhoto = function (id) {
--- js/util.js
+++ js/util.js
@@ -3,3 +3,12 @@ function checkStringLength(string, minLength, maxLength) {
 }
 
 checkStringLength('ddd', 6, 6);
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+getRandomIntInclusive(1, 9); //функция, возвращающая случайное целое число из переданного диапазона включительно взята из developer.mozilla.org
+
+export {getRandomIntInclusive};
